cafc0eeab408b6c59e4c4429fb02f0fdc270ec87
Jessica Sillus <153012636+jessicasillus@users.noreply.github.com>
2026-03-31 13:59:30 -0400

    Add files via upload

---
+++ ESP32 Programs/MPR121_Editor/MPR121_Editor.ino
@@ -0,0 +1,73 @@
+#include <Adafruit_MPR121.h>
+#include <Wire.h>
+
+#ifndef _BV
+#define _BV(bit) (1 << (bit))
+#endif
+
+Adafruit_MPR121 cap = Adafruit_MPR121();
+
+const int NUM_SENSORS = 9;  // (A1-A3, B1-B3, C1-C3)
+const int WINDOW_SIZE = 10;
+int history[NUM_SENSORS][WINDOW_SIZE];
+int readIndex = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+  if (!cap.begin(0x5A)) {
+    Serial.println("MPR121 not found!");
+    while (1);
+  }
+
+  // Reconfig to match sensors in 60-500pF range
+  cap.writeRegister(0x5E, 0x00);  // Stop mode
+  cap.writeRegister(0x7B, 0x00);  // Auto-Config Control 0
+  cap.writeRegister(0x7D, 0xE4);  // USL
+  cap.writeRegister(0x7E, 0x94);  // LSL
+  cap.writeRegister(0x7F, 0xC8);  // Target Level
+
+  cap.writeRegister(0x5C, 0x16);  // charge current
+  cap.writeRegister(0x5D, 0x60);  // charge time
+
+  // Drift fix
+  cap.writeRegister(0x2B, 0x01);  // MHD Rising
+  cap.writeRegister(0x2C, 0x01);  // NHD Rising
+  cap.writeRegister(0x2D, 0x00);  // NCL Rising  (no downward drift)
+  cap.writeRegister(0x2E, 0x00);  // FDL Rising
+  cap.writeRegister(0x2F, 0x01);  // MHD Falling (fast recovery)
+  cap.writeRegister(0x30, 0x01);  // NHD Falling
+
+  // enable NUM_SENSORS electrodes (ELE0 through ELE8)
+  cap.writeRegister(0x5E, 0x80 | NUM_SENSORS);
+
+  // start smoothing buffer with real sensor readings for better avgs
+  for (int i = 0; i < WINDOW_SIZE; i++) {
+    for (int sensor = 0; sensor < NUM_SENSORS; sensor++) {
+      history[sensor][i] = cap.filteredData(sensor);
+    }
+  }
+}
+
+void loop() {
+  for (int sensor = 0; sensor < NUM_SENSORS; sensor++) {
+    history[sensor][readIndex] = cap.filteredData(sensor);
+  }
+  readIndex = (readIndex + 1) % WINDOW_SIZE;
+
+  for (int sensor = 0; sensor < NUM_SENSORS; sensor++) {
+    long sum = 0;
+    for (int i = 0; i < WINDOW_SIZE; i++) {
+      sum += history[sensor][i];
+    }
+    Serial.print(sum / WINDOW_SIZE);
+    if (sensor < NUM_SENSORS - 1) {
+      Serial.print(","); // print to terminal for python to use later
+    }
+  }
+
+  Serial.print(",");
+  Serial.println();
+
+  delay(20);
+}
